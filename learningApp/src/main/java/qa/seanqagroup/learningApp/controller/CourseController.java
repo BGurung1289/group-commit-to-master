41b531fe3c76d7f40c00603a45f754ab292ca9cd
nelvinProjects <nelvin.joseph@academytrainee.com>
2018-09-14 14:55:38 +0100

    nelvin-my work so far

---
+++ learningApp/src/main/java/qa/seanqagroup/learningApp/controller/CourseController.java
@@ -0,0 +1,25 @@
+package qa.seanqagroup.learningApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import qa.seanqagroup.learningApp.model.Course;
+import qa.seanqagroup.learningApp.repository.CourseRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class CourseController {
+
+	@Autowired
+	private CourseRepository courseRepository;
+
+	@PostMapping("/course/add")
+	public void createCourse(Course course, @RequestParam("madeByTrainerId") Long madeByTrainerId) {
+		course.setTrainerId(madeByTrainerId);
+		courseRepository.save(course);
+	}
+}
