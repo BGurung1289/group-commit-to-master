47891f3daeb9abf8881944c4832dd8fd527003da
B <Bibek.Gurung@academytrainee.com>
2018-09-14 16:26:58 +0100

    bibek-my classes

---
+++ learningApp/src/test/java/qa/seanqagroup/learningApp/integration/UserControllerTest.java
@@ -0,0 +1,46 @@
+package integration;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import qa.seanqagroup.learningApp.LearningAppApplication;
+import qa.seanqagroup.learningApp.controller.UserController;
+import qa.seanqagroup.learningApp.repository.UserRepository;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {LearningAppApplication.class})
+@AutoConfigureMockMvc
+public class UserControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	
+	@Test
+	public void loginToDatabase() throws Exception{
+		mvc.perform(MockMvcRequestBuilders.post("/login")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("email","b@b.b")
+				.param("password", "pass")
+				) 
+		.andExpect(status() 
+				.isOk()); 
+	}
+}
+
