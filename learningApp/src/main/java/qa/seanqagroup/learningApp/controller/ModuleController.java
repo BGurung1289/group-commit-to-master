61c10b05da447d090025d38f30de55bfce0e2050
slacki123 <sebastian.lacki@academytrainee.com>
2018-09-14 15:25:31 +0100

    userTakesCourse and related controllers and repositories

--- learningApp/src/main/java/qa/seanqagroup/learningApp/controller/ModuleController.java
+++ learningApp/src/main/java/qa/seanqagroup/learningApp/controller/ModuleController.java
@@ -1,22 +1,41 @@
 package qa.seanqagroup.learningApp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import qa.seanqagroup.learningApp.model.Course;
 import qa.seanqagroup.learningApp.model.Module;
+import qa.seanqagroup.learningApp.model.Section;
+import qa.seanqagroup.learningApp.repository.CourseRepository;
 import qa.seanqagroup.learningApp.repository.ModuleRepository;
+import qa.seanqagroup.learningApp.repository.SectionRepository;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@RequestMapping("/module")
 public class ModuleController {
-
+	
 	@Autowired
-	private ModuleRepository moduleRepository;
-
-	@PostMapping("/module/add")
-	public void createModule(Module module) {
-		moduleRepository.save(module);
+	private ModuleRepository moduleRepo;
+	
+	@Autowired
+	private SectionRepository sectionRepo;
+	
+	@GetMapping("/{moduleId}/getSections") 
+	public List<Section> getNumberOfSections(@PathVariable(value = "moduleId") Long moduleId) {
+		// h'mta modulidt
+		Module module = moduleRepo.getModuleByModuleId(moduleId);
+		
+		//h'mta samtliga sections som tillh;r modulen
+		List<Section> sections = sectionRepo.getSectionsByModule(module);
+		
+		return sections;
 	}
+
 }
--- learningApp/src/main/java/qa/seanqagroup/learningApp/repository/SectionRepository.java
+++ learningApp/src/main/java/qa/seanqagroup/learningApp/repository/SectionRepository.java
@@ -1,11 +1,20 @@
 package qa.seanqagroup.learningApp.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import qa.seanqagroup.learningApp.model.Module;
 import qa.seanqagroup.learningApp.model.Section;
 
 @Repository
-public interface SectionRepository extends JpaRepository<Section,Long> {
+public interface SectionRepository extends JpaRepository<Section, Long> {
+	//Page<Section> getSectionsByModule(Module module, Pageable pageable);
 
+	List<Section> getSectionsByModule(Module module);
+	
+	Section getSectionBySectionId(Long sectionId);
 }
