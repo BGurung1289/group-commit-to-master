e922e301b46db78232be756c2554db9fbb6388ea
nelvinProjects <nelvin.joseph@academytrainee.com>
2018-09-18 10:24:39 +0100

    Done tests for GET all courses, modules and sections.

--- learningApp/src/test/java/qa/seanqagroup/learningApp/integration/ModuleControllerTest.java
+++ learningApp/src/test/java/qa/seanqagroup/learningApp/integration/ModuleControllerTest.java
@@ -1,8 +1,8 @@
 package qa.seanqagroup.learningApp.integration;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,13 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-
 import qa.seanqagroup.learningApp.LearningAppApplication;
-import qa.seanqagroup.learningApp.repository.ModuleRepository;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { LearningAppApplication.class })
@@ -58,6 +54,17 @@ public class ModuleControllerTest {
 			test.pass("Added module to database");
 		} catch (AssertionError e) {
 			test.fail("Didn't add module to database");
+		}
+	}
+
+	@Test
+	public void getCoursesTest() throws Exception {
+		test = extent.createTest("CourseController GET all courses");
+		try {
+			mvc.perform(MockMvcRequestBuilders.get("/course/searchCourse")).andExpect(status().isOk());
+			test.pass("GET all course details as JSON array");
+		} catch (Exception e) {
+			test.fail("Failed to GET all course details as JSON array");
 		}
 	}
 
